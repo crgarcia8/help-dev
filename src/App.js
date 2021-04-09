de1463ea219e8279d24d52971faf2ba348bd6e6a
crgarcia8 <crgarcia8@gmail.com>
2021-04-09 18:16:31 -0500

    Modify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React(NEW PUSH)
+          Learn React(NEW PUSH22)
         </a>
       </header>
     </div>
